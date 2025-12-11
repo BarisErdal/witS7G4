16a734e964356766ef4b2a5e8d1c7ac8a4d8764e
BarisErdal <bariserdal.bme@gmail.com>
2025-12-11 16:43:34 +0300

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 import Login from '../components/Login'
-import Success from '../components/Success'
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>Hello</div>
-      <Login/>
+      
+      <button>DENEME</button>
        
     </>
   )
