cedd4ab003964594ba0298a7af55e901add453f4
BarisErdal <bariserdal.bme@gmail.com>
2025-12-11 16:36:25 +0300

    login eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Login from '../components/Login'
+import Success from '../components/Success'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>Hello</div>
-        
+      <Login/>
+       
     </>
   )
 }
